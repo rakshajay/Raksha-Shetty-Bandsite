ed1a0cdbf4eb2cefbd59ee405187911e8398cb00
rakshajay <rakshashettyhs@gmail.com>
2024-08-05 11:35:12 -0700

    Shows mobile completed

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,4 +1,4 @@
-let shows=[
+let shows = [
     {
         date: "Mon Sept 09 2024",
         venue: "Ronald Lane",
@@ -7,110 +7,127 @@ let shows=[
     {
         date: "Tue Sept 17 2024",
         venue: "Pier 3 East",
-        location: "San Francisco, CA", 
+        location: "San Francisco, CA",
     },
     {
         date: "Sat Oct 12 2024",
         venue: "View Lounge ",
-        location: "San Francisco, CA", 
+        location: "San Francisco, CA",
     },
     {
         date: "Sat Nov 16 2024",
         venue: "Hyatt Agency",
-        location: "San Francisco, CA", 
+        location: "San Francisco, CA",
     },
     {
         date: "Fri Nov 29 2024",
         venue: "Moscow Center",
-        location: "San Francisco, CA", 
+        location: "San Francisco, CA",
     },
     {
         date: "Wed Dec 18 2024",
         venue: "Press Club",
-        location: "San Francisco, CA", 
+        location: "San Francisco, CA",
     }
 ]
 
-const showCard = document.querySelector(".shows-container");
+const showCard = document.querySelector(".shows");
 
-function renderShowList(){
+function renderShowList() {
 
- for(i=0; i<shows.length; i++){
-    const showList = document.createElement("li");
-    showList.classList.add("show-list");
+    const tittleContainer = document.createElement("div");
+    tittleContainer.classList.add("shows-title");
+    const tittle = document.createElement("h1");
+    tittle.innerText = "Shows";
 
-    /*---------Date-Container-----------*/
-    
-    const dateContainer = document.createElement("div");
-    dateContainer.classList.add("day-div");
+    const showListContainer = document.createElement("div");
+    showListContainer.classList.add("shows-list");
 
-    const dateHeading = document.createElement("h2");
-    dateHeading.classList.add("date-heading");
-    dateHeading.innerText = "DATE";
-
-    const date = document.createElement("h2");
-    date.classList.add("date-tag");
-    date.innerText = shows[i].date;
-
-    /*---------Venue-Container-----------*/
-
-    const venueContainer = document.createElement("div");
-    venueContainer.classList.add("venue-div");
-
-    const venueHeading = document.createElement("h2");
-    venueHeading.classList.add("venue-heading");
-    venueHeading.innerText = "VENUE";
-
-    const venue = document.createElement("h2");
-    venue.classList.add("venue-tag");
-    venue.innerText = shows[i].venue;
-
-    /*---------Location-Container-----------*/
-
-    const locationContainer = document.createElement("div");
-    locationContainer.classList.add("location-div");
-
-    const locationHeading = document.createElement("h2");
-    locationHeading.classList.add("location-heading");
-    locationHeading.innerText = "LOCATION";
-
-    const location = document.createElement("h2");
-    location.classList.add("location-tag");
-    location.innerText = shows[i].location;
-
-    /*---------Button-Container-----------*/
-
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("button-div");
-
-    const buttonText = document.createElement("button");
-    buttonText.classList.add("button-text");
-    buttonText.innerText = "BUY TICKETS";
-
-
-
+    const unOrderedListContainer = document.createElement("ul");
     
 
-    showCard.appendChild(showList);
+    for (i = 0; i < shows.length; i++) {
 
-    showList.appendChild(dateContainer);
-    dateContainer.appendChild(dateHeading);
-    dateContainer.appendChild(date);
+        /*---------List-Container-----------*/
 
-    showList.appendChild(venueContainer);
-    venueContainer.appendChild(venueHeading);
-    venueContainer.appendChild(venue);
+        const showList = document.createElement("li");
+        showList.classList.add("shows-list__li");
 
-    showList.appendChild(locationContainer);
-    locationContainer.appendChild(locationHeading);
-    locationContainer.appendChild(location);
-    
-    showList.appendChild(buttonContainer);
-    locationContainer.appendChild(buttonText);
+        /*---------Date-Container-----------*/
 
- 
- }
-    
+        const dateContainer = document.createElement("div");
+        dateContainer.classList.add("shows-list__li-item");
+
+        const dateHeading = document.createElement("h2");
+        dateHeading.innerText = "DATE";
+
+        const date = document.createElement("h3");
+        date.setAttribute("id", "date-id");
+        date.innerText = shows[i].date;
+
+        /*---------Venue-Container-----------*/
+
+        const venueContainer = document.createElement("div");
+        venueContainer.classList.add("shows-list__li-item");
+
+        const venueHeading = document.createElement("h2");
+
+        venueHeading.innerText = "VENUE";
+
+        const venue = document.createElement("h3");
+
+        venue.innerText = shows[i].venue;
+
+        /*---------Location-Container-----------*/
+
+        const locationContainer = document.createElement("div");
+        locationContainer.classList.add("shows-list__li-item");
+
+        const locationHeading = document.createElement("h2");
+
+        locationHeading.innerText = "LOCATION";
+
+        const location = document.createElement("h3");
+
+        location.innerText = shows[i].location;
+
+        /*---------Button-Container-----------*/
+
+        const buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("shows-list__li-button");
+
+        const buttonText = document.createElement("button");
+       
+        buttonText.innerText = "BUY TICKETS";
+
+
+
+        showCard.appendChild(tittleContainer);
+        tittleContainer.appendChild(tittle);
+
+        showCard.appendChild(showListContainer);
+
+        showListContainer.appendChild(unOrderedListContainer);
+        unOrderedListContainer.appendChild(showList);
+
+        showList.appendChild(dateContainer);
+        dateContainer.appendChild(dateHeading);
+        dateContainer.appendChild(date);
+
+        showList.appendChild(venueContainer);
+        venueContainer.appendChild(venueHeading);
+        venueContainer.appendChild(venue);
+
+        showList.appendChild(locationContainer);
+        locationContainer.appendChild(locationHeading);
+        locationContainer.appendChild(location);
+
+        showList.appendChild(buttonContainer);
+        buttonContainer.appendChild(buttonText);
+
+
+    }
+
 
 
 }
